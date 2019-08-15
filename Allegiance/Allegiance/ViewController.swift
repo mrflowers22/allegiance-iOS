ba4ec0b665217beaf63f1cf2d68cd81ee3c8122f
Michael R.F <mrflowers22@uwalumni.com>
2019-08-15 08:27:54 -0700

    first commit

---
+++ Allegiance/Allegiance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Allegiance
+//
+//  Created by Michael Flowers on 8/15/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
